4154900417e095e29d7ca062e8fc185790853f28
JTkeio <jacobtkeio@gmail.com>
2021-08-31 00:07:43 -0700

    fix y value constraint in drawBezierProfile() to 412 instead of 557, write a preliminary fitBezierProfileToSelf() function (subject to change!), and streamline BezierProfileManagerTest.main()

--- src/main/kotlin/jtkeio/bezier/BezierProfileManager.kt
+++ src/main/kotlin/jtkeio/bezier/BezierProfileManager.kt
@@ -5,22 +5,56 @@ import kotlin.math.*
 
 class BezierProfileManager(val field: FRCField) {
 
-    val obstacleAvoidanceWaypoints = ArrayList<Int>()
 
 
-    fun generateBezierProfile(currentPosition: Pair<Double, Double>, targetPosition: Pair<Double, Double>, currentHeading: Double = 180*atan((targetPosition.second-currentPosition.second) / (targetPosition.first-currentPosition.first))/PI, targetHeading: Double = 180*atan((targetPosition.second-currentPosition.second) / (targetPosition.first-currentPosition.first))/PI): BezierProfile {
-        if (currentPosition.first<0 || currentPosition.second<0) {
-            throw IllegalArgumentException("currentPosition in function call $this.generateBezierProfile() cannot be a negative number (passed $currentPosition)")
-        } else if (targetPosition.first<0 || targetPosition.first<0) {
-            throw IllegalArgumentException("targetPosition in function call $this.generateBezierProfile() cannot be a negative number (passed $targetPosition)")
-        } else if (currentPosition.first>8.2 || currentPosition.second>16.6) {
-            throw IllegalArgumentException("currentPosition in function call $this.generateBezierProfile() cannot have coordinates greater than (8.1, 16.5) (passed $currentPosition)")
-        } else if (targetPosition.first>8.2 || targetPosition.second>16.6) {
-            throw IllegalArgumentException("targetPosition in function call $this.generateBezierProfile() cannot have coordinates greater than (8.1, 16.5) (passed $targetPosition)")
-        }
+
+    fun generateBezierProfile(currentPosition: Pair<Double, Double>, targetPosition: Pair<Double, Double>, currentHeading: Double = 180*atan2(targetPosition.second-currentPosition.second,targetPosition.first-currentPosition.first)/PI, targetHeading: Double = 180*atan2(targetPosition.second-currentPosition.second, targetPosition.first-currentPosition.first)/PI): BezierProfile {
+        if (currentPosition.first<0 || currentPosition.second<0) { throw IllegalArgumentException("currentPosition in function call $this.generateBezierProfile() cannot be a negative number (passed $currentPosition)")
+        } else if (targetPosition.first<0 || targetPosition.first<0) { throw IllegalArgumentException("targetPosition in function call $this.generateBezierProfile() cannot be a negative number (passed $targetPosition)")
+        } else if (currentPosition.first>8.2 || currentPosition.second>16.6) { throw IllegalArgumentException("currentPosition in function call $this.generateBezierProfile() cannot have coordinates greater than (8.1, 16.5) (passed $currentPosition)")
+        } else if (targetPosition.first>8.2 || targetPosition.second>16.6) { throw IllegalArgumentException("targetPosition in function call $this.generateBezierProfile() cannot have coordinates greater than (8.1, 16.5) (passed $targetPosition)") }
         val guidePointMultiplier = sqrt((targetPosition.second-currentPosition.second).pow(2) + (targetPosition.first-currentPosition.first).pow(2)) * 0.6
         val currentGuidePoint = currentPosition.first + cos(PI*currentHeading/180)*guidePointMultiplier to currentPosition.second + sin(PI*currentHeading/180)*guidePointMultiplier
-        val targetGuidePoint = targetPosition.first + cos(PI*(targetHeading - 180)/180)*guidePointMultiplier to targetPosition.second + sin(PI*(targetHeading - 180)/180)*guidePointMultiplier
-        return BezierProfile(arrayOf(currentPosition, currentGuidePoint, targetGuidePoint, targetPosition))
+        val targetGuidePoint = targetPosition.first + cos(PI*(180 + targetHeading)/180)*guidePointMultiplier to targetPosition.second + sin(PI*(180 + targetHeading)/180)*guidePointMultiplier
+        return BezierProfile(arrayOf(currentPosition, currentGuidePoint, targetGuidePoint, targetPosition)).fitBezierProfileToSelf()
+    }
+
+    private fun BezierProfile.fitBezierProfileToSelf(): BezierProfile {
+        val bezierProfileTestPoints = Array(256) { this.calculateBezierPoint(it / 256.toDouble()) }
+        for (a in bezierProfileTestPoints.indices) {
+            for (b in bezierProfileTestPoints.indices) {
+                if (a==b) {continue}
+                if (sqrt((bezierProfileTestPoints[a].first - bezierProfileTestPoints[b].first).pow(2) + (bezierProfileTestPoints[a].second - bezierProfileTestPoints[b].second).pow(2)) < 0.01) {
+                    val idealBezierCurveAngle = (this.derivativeBezierProfile.calculateIntegralBezierAngle(a / 256.toDouble()) + 360) % 180
+                    val testBezierCurveAngle = (180 * atan2(y = bezierProfileTestPoints[a].second - bezierProfileTestPoints[b].second, x = bezierProfileTestPoints[a].first - bezierProfileTestPoints[b].first)/PI + 360) % 180
+                    if (testBezierCurveAngle + 30 < idealBezierCurveAngle || testBezierCurveAngle - 30 > idealBezierCurveAngle) {
+                        val insertIndex = waypoints.size - 2
+                        val fittedWaypoints = Array(waypoints.size+1) {
+                            if (it < insertIndex) {
+                                waypoints[it]
+                            } else if (it > insertIndex) {
+                                waypoints[it-1]
+                            } else {
+                                val newWaypointAngle = PI + atan2(abs(waypoints[0].second-bezierProfileTestPoints[a].second), abs(waypoints[0].first-bezierProfileTestPoints[a].first))
+                                val newWaypointMultipliers = abs(waypoints[0].first-waypoints[waypoints.lastIndex].first)/2 to abs(waypoints[1].second-waypoints[waypoints.lastIndex].second)/2
+                                val x = bezierProfileTestPoints[a].first + cos(newWaypointAngle)*newWaypointMultipliers.second
+                                val y = bezierProfileTestPoints[a].second + sin(newWaypointAngle)*newWaypointMultipliers.first
+                                x to y
+                            }
+                        }
+                        return BezierProfile(fittedWaypoints)
+                    }
+                }
+            }
+        }
+        return this
+    }
+
+    fun fitBezierProfileToBoundaries(inputBezierProfile: BezierProfile) {
+
+    }
+
+    fun fitBezierProfileToObstacles(inputBezierProfile: BezierProfile) {
+
     }
 }
--- src/test/kotlin/jtkeio/bezier/BezierProfileManagerTest.kt
+++ src/test/kotlin/jtkeio/bezier/BezierProfileManagerTest.kt
@@ -12,24 +12,27 @@ fun main() {
         (8.1 to 16.5) to (0.0 to 16.5),
         (0.0 to 16.5) to (0.0 to 0.0)
     )
-    field.addObstacle((2.0 to 4.0) to (4.0 to 4.5))
+    field.addObstacle((4.0 to 4.0) to (4.0 to 5.0))
     val fieldImage = field.drawField()
     val manager = BezierProfileManager(field)
 
-    val frame = JFrame("BezierProfileManagerTest")
-    val label = JLabel()
-    label.setSize(256, 512)
-    label.isVisible = true
-    frame.setSize(272, 528)
-    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-    frame.add(label)
-    frame.isVisible = true
+    val label = JLabel().apply {
+        setSize(256, 512)
+        isVisible = true
+    }
+    JFrame("BezierProfileManagerTest").apply {
+        setSize(272, 528)
+        add(label)
+        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        isVisible = true
+    }
 
     while (true) {
         for (i in 0 until 359) {
-            val currentImage = manager.generateBezierProfile(2.0 to 2.0, 7.0 to 15.0, i.toDouble()).drawBezierProfile(fieldImage)
+            val baseProfile = manager.generateBezierProfile(4.0 to 3.0, 2.0 to 11.0, i.toDouble())
+            val currentImage = baseProfile.drawBezierProfile(fieldImage)
             label.icon = ImageIcon(currentImage)
-            Thread.sleep(40)
+            Thread.sleep(30)
         }
     }
 }
